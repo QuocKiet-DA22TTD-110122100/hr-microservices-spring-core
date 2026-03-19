6adf25217551225e15a2d505c563ea6fe79c94e6
HuynhQuocKiet <110122100@st.tvu.edu.vn>
2026-03-19 08:47:33 +0700

    Delete hr-service directory

--- hr-service/src/main/java/com/example/hrservice/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.hrservice.repository;
-
-import com.example.hrservice.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
